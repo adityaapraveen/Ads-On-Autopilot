7e886606533f5b3e855e904b6a6341c67fef3c15
adityaapraveen-intervueio <aditya.praveen@intervue.io>
2026-03-26 20:35:39 +0530

    feat: initial project setup

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT 
+
+app.get('/health', (req, res) => {
+    res.json({status:'ok', message:"Backend Running :)"})
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server running at port: ${PORT}}`)
+})
